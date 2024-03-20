a82871340d940119789d31384c9b6d08769c8421
Sara Girard <sarag.bsa@gmail.com>
2024-03-20 09:06:55 -0400

    python lint 4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,7 +16,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params+key+"="+value+"&"
+            params= params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -25,10 +25,11 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
-        print(f"Network exception occurred")
-        
+        print(f"Network exception occurred: {e}")
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -39,11 +40,12 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
@@ -18,11 +17,14 @@ urlpatterns = [
 
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', 
+    path(
+        route='get_dealers/<str:state>',
         view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', 
+    path(
+        route='dealer/<int:dealer_id>',
         view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', 
+    path(
+        route='reviews/dealer/<int:dealer_id>',
         view=views.get_dealer_reviews, name='dealer_details'),
     
     # path for add a review view
